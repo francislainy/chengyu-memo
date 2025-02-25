2f7cd0b694380e0f95e22861a7b58ce5f8cfdfef
Francislainy <francislainy.campos@gmail.com>
2025-02-25 02:55:47 +0000

    Add model for list of phrases

--- src/app/components/chengyu-list/chengyu-list.component.ts
+++ src/app/components/chengyu-list/chengyu-list.component.ts
@@ -19,7 +19,8 @@ export class ChengyuListComponent {
       pinyin: "zhǎn dīng jié tiě",
       image: "zhandingjietie-斩钉截铁.png",
       translation: "cutting nails and slicing iron.",
-      description: "It's used to describe a person who is very decisive and resolute, with an attitude of firmness and determination. Imagine someone who makes decisions with such conviction that it's like they're cutting through nails and iron—there's no hesitation or doubt."
+      description: "It's used to describe a person who is very decisive and resolute, with an attitude of firmness and determination. Imagine someone who makes decisions with such conviction that it's like they're cutting through nails and iron—there's no hesitation or doubt.",
+      phrases: ["phrase1", "phrase2", "phrase3"]
     },
     {
       id: 2,
@@ -27,23 +28,26 @@ export class ChengyuListComponent {
       pinyin: "yī jiàn shuāng diāo",
       image: "yijianshuangdiao-箭双雕.png",
       translation: "one arrow, two eagles.",
-      description: "It means achieving two goals with a single action, much like the English phrase \"killing two birds with one stone.\" Imagine a skilled archer releasing a single arrow that hits two eagles at once—it's all about efficiency and skill."
+      description: "It means achieving two goals with a single action, much like the English phrase \"killing two birds with one stone.\" Imagine a skilled archer releasing a single arrow that hits two eagles at once—it's all about efficiency and skill.",
+      phrases: ["phrase1", "phrase2", "phrase3"]
     },
     {
       id: 3,
-      chengyu: "斩钉截铁",
-      pinyin: "zhǎn dīng jié tiě",
-      image: "zhandingjietie-斩钉截铁.png",
-      translation: "cutting nails and slicing iron.",
-      description: "It's used to describe a person who is very decisive and resolute, with an attitude of firmness and determination. Imagine someone who makes decisions with such conviction that it's like they're cutting through nails and iron—there's no hesitation or doubt."
+      chengyu: "不可思议",
+      pinyin: "bù kě sī yì",
+      image: "bukesiyi-不可思议.png",
+      translation: "unimaginable, incredible.",
+      description: "It's used to describe something that is beyond ordinary comprehension, astonishing, or extraordinary. Imagine witnessing a breathtaking natural phenomenon or an unbelievable feat—it's something so incredible that it defies belief.",
+      phrases: ["phrase1", "phrase2", "phrase3"]
     },
     {
       id: 4,
       chengyu: "斩钉截铁",
       pinyin: "zhǎn dīng jié tiě",
       image: "yijianshuangdiao-箭双雕.png",
-      translation: "cutting nails and slicing iron.",
-      description: "It's used to describe a person who is very decisive and resolute, with an attitude of firmness and determination. Imagine someone who makes decisions with such conviction that it's like they're cutting through nails and iron—there's no hesitation or doubt."
+      translation: "unimaginable, incredible.",
+      description: "It's used to describe a person who is very decisive and resolute, with an attitude of firmness and determination. Imagine someone who makes decisions with such conviction that it's like they're cutting through nails and iron—there's no hesitation or doubt.",
+      phrases: ["phrase1", "phrase2", "phrase3"]
     },
     {
       id: 5,
@@ -51,15 +55,17 @@ export class ChengyuListComponent {
       pinyin: "zhǎn dīng jié tiě",
       image: "zhandingjietie-斩钉截铁.png",
       translation: "cutting nails and slicing iron.",
-      description: "It's used to describe a person who is very decisive and resolute, with an attitude of firmness and determination. Imagine someone who makes decisions with such conviction that it's like they're cutting through nails and iron—there's no hesitation or doubt."
+      description: "It's used to describe a person who is very decisive and resolute, with an attitude of firmness and determination. Imagine someone who makes decisions with such conviction that it's like they're cutting through nails and iron—there's no hesitation or doubt.",
+      phrases: ["phrase1", "phrase2", "phrase3"]
     },
     {
       id: 6,
       chengyu: "斩钉截铁",
       pinyin: "zhǎn dīng jié tiě",
-      image: "yijianshuangdiao-箭双雕.png",
+      image: "bukesiyi-不可思议.png",
       translation: "cutting nails and slicing iron.",
-      description: "It's used to describe a person who is very decisive and resolute, with an attitude of firmness and determination. Imagine someone who makes decisions with such conviction that it's like they're cutting through nails and iron—there's no hesitation or doubt."
+      description: "It's used to describe a person who is very decisive and resolute, with an attitude of firmness and determination. Imagine someone who makes decisions with such conviction that it's like they're cutting through nails and iron—there's no hesitation or doubt.",
+      phrases: ["phrase1", "phrase2", "phrase3"]
     },
     {
       id: 7,
@@ -67,15 +73,17 @@ export class ChengyuListComponent {
       pinyin: "zhǎn dīng jié tiě",
       image: "yijianshuangdiao-箭双雕.png",
       translation: "cutting nails and slicing iron.",
-      description: "It's used to describe a person who is very decisive and resolute, with an attitude of firmness and determination. Imagine someone who makes decisions with such conviction that it's like they're cutting through nails and iron—there's no hesitation or doubt."
+      description: "It's used to describe a person who is very decisive and resolute, with an attitude of firmness and determination. Imagine someone who makes decisions with such conviction that it's like they're cutting through nails and iron—there's no hesitation or doubt.",
+      phrases: ["phrase1", "phrase2", "phrase3"]
     },
     {
       id: 8,
       chengyu: "斩钉截铁",
       pinyin: "zhǎn dīng jié tiě",
-      image: "yijianshuangdiao-箭双雕.png",
+      image: "bukesiyi-不可思议.png",
       translation: "cutting nails and slicing iron.",
-      description: "It's used to describe a person who is very decisive and resolute, with an attitude of firmness and determination. Imagine someone who makes decisions with such conviction that it's like they're cutting through nails and iron—there's no hesitation or doubt."
+      description: "It's used to describe a person who is very decisive and resolute, with an attitude of firmness and determination. Imagine someone who makes decisions with such conviction that it's like they're cutting through nails and iron—there's no hesitation or doubt.",
+      phrases: ["phrase1", "phrase2", "phrase3"]
     },
   ]
 }
--- src/app/models/Chengyu.model.ts
+++ src/app/models/Chengyu.model.ts
@@ -5,4 +5,5 @@ export interface IChengyu {
   translation: string,
   description: string,
   image: string,
+  phrases?: string[]
 }
