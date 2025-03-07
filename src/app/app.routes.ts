a09ddcf97f9ead4fce88a4a27bbb201f56fa6e42
Francislainy <francislainy.campos@gmail.com>
2025-03-07 15:50:43 +0000

    Remove home route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,5 @@ import {Routes} from '@angular/router';
 import {HomeComponent} from './components/home/home.component';
 
 export const routes: Routes = [
-  {path: '', pathMatch: 'full', redirectTo: 'home'},
   {path: '', component: HomeComponent}
 ];
